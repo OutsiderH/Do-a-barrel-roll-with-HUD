b0565b4b890029b609ae6ab74bd2b14580bc1732
OutsiderH <huanghaoranxh@gmail.com>
2024-02-14 13:19:09 +0800

    add dependencies

---
+++ src/client/java/com/github/outsider/doabarrelrollwithhud/DoABarrelRollWithHudClient.java
@@ -0,0 +1,10 @@
+package com.github.outsider.doabarrelrollwithhud;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DoABarrelRollWithHudClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		
+	}
+}
