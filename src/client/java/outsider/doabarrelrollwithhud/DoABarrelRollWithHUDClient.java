eec640ad3628a7938da736df48b5cdd8f59f7f60
OutsiderH <huanghaoranxh@gmail.com>
2024-02-14 10:02:51 +0800

    init

---
+++ src/client/java/outsider/doabarrelrollwithhud/DoABarrelRollWithHUDClient.java
@@ -0,0 +1,10 @@
+package outsider.doabarrelrollwithhud;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DoABarrelRollWithHUDClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
